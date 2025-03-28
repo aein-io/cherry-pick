779559d4ec5f1535a406aed1d6663d4b1102727f
aein <aaeinnor11@gmail.com>
2025-03-28 15:23:23 +0800

    updated main.py
    - handles value errors
    - handles other exceptions
    - dynamic url input

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+import argparse
 from recipe_scrapers import scrape_me, WebsiteNotImplementedError
 from cherry_pick.recipe_to_md import generate_recipe_text
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Scrape a recipe from a given URL and return it as a PDF file.")
+    parser.add_argument("url", help="The URL of the recipe to scrape")
+
+    args = parser.parse_args()
+
     try:
-        scraper = scrape_me(
-            "https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/")
+        scraper = scrape_me(str(args.url))
         md_text = generate_recipe_text(scraper)
         print(md_text)
     except WebsiteNotImplementedError:
-        print("Sorry! The website is currently not supported by recipe-scrapers 😔")
+        print("Sorry! The website is currently not supported by recipe-scrapers.")
+    except ValueError:
+        print("Oops! The input is not a valid url.")
+    except Exception:
+        print("Oops! URL could not be accessed, please use a different website.")
 
 
 if __name__ == "__main__":
