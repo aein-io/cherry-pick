00bed218da5cd0520e38a0305ebc27e618bcf919
aein <aaeinnor11@gmail.com>
2025-03-29 21:24:36 +0800

    updated tests

--- test/test_recipe_handlers.py
+++ test/test_recipe_handlers.py
@@ -1,13 +1,18 @@
 import pytest
 from recipe_scrapers import scrape_me, WebsiteNotImplementedError
+from cherry_pick.recipe_handlers import scrape_recipe, process_recipe
 
 
 class TestErrors:
     def test_websitenotimplemented_err(self):
         with pytest.raises(WebsiteNotImplementedError):
-            scrape_me(
+            scrape_recipe(
                 "https://iwashyoudry.com/last-minute-chicken-recipe/")
+
+    def test_value_err(self):
+        with pytest.raises(ValueError):
+            scrape_recipe("asdfghjkl")
 
     def test_exception(self):
         with pytest.raises(Exception):
-            scrape_me("https://www.teaforturmeric.com/chicken-biryani/")
+            scrape_recipe("https://www.teaforturmeric.com/chicken-biryani/")
